06e81e6027beb38d410695ab3532cc1fd42ae858
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-10-04 17:14:44 +0800

    Update scriptlib.js

--- scriptlib.js
+++ scriptlib.js
@@ -1,7 +1,13 @@
 var para=document.createElement("script");
-if (_ver == "newest") {para.src="https://mwashfds.github.io/SciptLib/0.3/scriptlib.js";}
-else {para.src="https://mwashfds.github.io/ScriptLib/" + _ver + "/scriptlib.js";}
+if (_ver == "newest") {
+ para.src="https://mwashfds.github.io/SciptLib/0.3/scriptlib.js";
+ console.log("Fetching newest version of ScriptLib...");
+}
+else {
+ para.src="https://mwashfds.github.io/ScriptLib/" + _ver + "/scriptlib.js";
+ console.log("Fetching ScriptLib of version " + _ver + " ...");
+}
 if (para.src !== null) {
- var element=document.body;
+ var element=document.head;
  element.appendChild(para);
 }
