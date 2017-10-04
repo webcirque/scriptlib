932b6cd9d05d64d0db493d398a7aac89f70fab0d
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-10-04 11:29:44 +0800

    Update scriptlib_loader.js

--- scriptlib_loader.js
+++ scriptlib_loader.js
@@ -1,6 +1,6 @@
 var para=document.createElement("script");
 if (_ver == "newest") {para.src="https://mwashfds.github.io/SciptLib/0.3/scriptlib.js";}
-else {para.src="https://mwashfds.github.io/SciptLib/" + _ver + "/scriptlib.js";}
+else {para.src="https://mwashfds.github.io/ScriptLib/" + _ver + "/scriptlib.js";}
 if (para.src !== null) {
  var element=document.body;
  element.appendChild(para);
