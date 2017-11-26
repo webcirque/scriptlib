a142ee9a8a2ee77009853d76160cd7369135e66c
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-11-26 13:35:24 +0800

    Update scriptlib.js

--- scriptlib.js
+++ scriptlib.js
@@ -1,4 +1,10 @@
 var para=document.createElement("script");
+try {
+ _g = _ver;
+}
+catch (err) {
+ _ver = "newest";
+}
 if (_ver == "newest") {
  para.src="https://mwashfds.github.io/ScriptLib/0.4/scriptlib.js";
  console.log("Fetching newest version of ScriptLib...");
