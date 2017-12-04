e2934c6dd1af0f893a3ed2176ea86a4e92725b15
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-12-04 17:49:01 +0800

    Update scriptlib.js

--- 0.4/scriptlib.js
+++ 0.4/scriptlib.js
@@ -73,6 +73,9 @@ _.set("msg.mwIsNotValid","MediaWiki is not included in this site.");
 _.set("msg.mwIsConstr","Hooking with MediaWiki...");
 _.set("msg.mwIsBuilt","Hooked with MediaWiki.");
 
+//List of HTML Tags
+_.set("htmlTag","a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,big,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,command,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,h1> - <h6,head,header,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,main,map,mark,menu,menuitem,meta,meter,nav,noframes,noscript,object,ol,optgroup,option,output,p,param,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,small,source,span,strike,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr".split(",");
+
 //Quick string methods
 _.s = function () {
 	console.error(_("msg.notAFunction").replace("$1","_.q"));
