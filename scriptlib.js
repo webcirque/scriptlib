d4afcc98e2bd5b0af48af9313b6cdf66ae5bd26e
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-06-18 12:50:39 +0800

    Update scriptlib.js

--- scriptlib.js
+++ scriptlib.js
@@ -12,11 +12,11 @@ catch (err) {
  _min = true;
 }
 if (_ver == "newest") {
- para.src="https://mwashfds.github.io/elsl/0.4/scriptlib.js";
+ para.src="https://mwashfds.github.io/scriptlib/0.4/scriptlib.js";
  console.log("Fetching newest version of ScriptLib...");
 }
 else {
- para.src="https://mwashfds.github.io/elsl/" + _ver + "/scriptlib.js";
+ para.src="https://mwashfds.github.io/scriptlib/" + _ver + "/scriptlib.js";
  console.log("Fetching ScriptLib of version " + _ver + " ...");
 }
 if (para.src !== null) {
@@ -27,10 +27,10 @@ css = document.createElement("link");
 css.rel = "stylesheet";
 css.type = "text/css";
 if (_min == true) {
- css.href = "https://mwashfds.github.io/elsl/dlib/elsl.min.css";
+ css.href = "https://mwashfds.github.io/scriptlib/dlib/elsl.min.css";
  console.log("ELSL minimized CSS file was requested.");
 } else if (_min == false) {
- css.href = "https://mwashfds.github.io/elsl/dlib/elsl.css";
+ css.href = "https://mwashfds.github.io/scriptlib/dlib/elsl.css";
  console.log("ELSL CSS file was requested.");
 }
 document.head.appendChild(css);
